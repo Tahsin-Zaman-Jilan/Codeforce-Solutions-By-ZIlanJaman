2404d61e346305361956446e523625a2fda3591b
Tahsin-Zaman-Jilan <77837621+Tahsin-Zaman-Jilan@users.noreply.github.com>
2021-02-02 19:45:08 +0600

    Create 4A. Watermelon.cpp

---
+++ 4A. Watermelon.cpp
@@ -0,0 +1,19 @@
+# Codeforce-Solutions-By-ZIlanJaman
+It is the solutions of some codeforce problems
+//4A. Watermelon
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+ int w;
+ cin>>w;
+ if (w%2==0 && w!=2){
+            cout<<("YES");
+ }
+ else{
+   cout<<("NO");
+ }
+
+
+}
+ 
