52d6d10161767f4f9c1f6bed61237e1c7c19bd14
Tahsin-Zaman-Jilan <77837621+Tahsin-Zaman-Jilan@users.noreply.github.com>
2021-02-13 21:52:18 +0600

    Create 158A. Next Round.cpp

---
+++ 158A. Next Round.cpp
@@ -0,0 +1,20 @@
+# Codeforce-Solutions-By-ZIlanJaman
+It is the solutions of some codeforce problems
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ int K,L,A,sum=0;
+ cin>>K>>L;
+ vector<int>v;
+ for( int i=0; i<K;i++) {
+    cin>>A;
+    v.push_back(A);
+ }
+    for(int i=0;i<K;i++){
+        if(v[i]>0 && v[i]>=v[L-1] ){
+            sum++;
+        }
+    }
+    cout<<sum;
+}
