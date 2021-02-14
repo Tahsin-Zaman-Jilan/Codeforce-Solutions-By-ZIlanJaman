200124e307e8bcf44cfa2b9d1b9c6dd12c1c0e14
Tahsin-Zaman-Jilan <77837621+Tahsin-Zaman-Jilan@users.noreply.github.com>
2021-02-14 11:02:39 +0600

    Create 50A. Domino piling.cpp

---
+++ 50A. Domino piling.cpp
@@ -0,0 +1,12 @@
+# Codeforce-Solutions-By-ZIlanJaman
+It is the solutions of some codeforce problems
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+int M,N,X;
+cin>>M>>N;
+X=(M*N)/2;
+cout<<X;
+
+}
