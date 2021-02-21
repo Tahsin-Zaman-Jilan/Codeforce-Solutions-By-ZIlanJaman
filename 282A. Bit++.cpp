4a232aea0e7a6d83c6757392ae9210692a542058
Tahsin-Zaman-Jilan <77837621+Tahsin-Zaman-Jilan@users.noreply.github.com>
2021-02-21 15:19:42 +0600

    Create 282A. Bit++.cpp

---
+++ 282A. Bit++.cpp
@@ -0,0 +1,19 @@
+# Codeforce-Solutions-By-ZIlanJaman
+It is the solutions of some codeforce problems
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+int n;
+int x=0;
+string X;
+cin>>n;
+for(int i=0;i<n;i++){
+cin>>X;
+if (X=="++X"){ x=x+1;}
+else if (X=="X--"){ x=x-1;}
+else if (X=="X++"){ x=x+1;}
+else if (X=="--X"){ x=x-1;}
+}
+cout<<x;
+}
